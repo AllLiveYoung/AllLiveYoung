e2335478b57f489296c3944323ee686a9f2bef21
AllLiveYoung <hansung0705@naver.com>
2021-12-15 16:03:13 +0900

    clock main edit

---
+++ a.js
@@ -0,0 +1,47 @@
+const clock = document.querySelector('.h1-clock');
+
+
+function getTime() {
+  const time = new Date();
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  //clock.innerHTML = hour +":" + minutes + ":"+seconds;
+  clock.innerHTML = `${hour < 10 ? `0${hour}` : hour}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+}
+
+
+function init() {
+  setInterval(getTime, 1000);
+  setInterval(typing, 200);
+}
+
+init();
+
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+  hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+})
+
+const content = "KEYU";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing() {
+  if (i < content.length) {
+    let txt = content.charAt(i);
+    text.innerHTML += txt;
+    i++;
+  }
+}
+
+
